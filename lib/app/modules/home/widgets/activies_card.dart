ac0360ab92eae1eb1bf960684d8eb79535ce7fba
Lucas Freitas <lucas.antunes@fiergs.org.br>
2022-08-30 01:12:33 -0300

    creating card of activities and finishing layout design

--- lib/app/modules/home/widgets/activies_card.dart
+++ lib/app/modules/home/widgets/activies_card.dart
@@ -5,12 +5,68 @@ class ActiviesCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 173.0,
-      decoration: const BoxDecoration(
-        color: Color(0xff4B4B4B),
-        borderRadius: BorderRadius.all(
-          Radius.circular(4.0),
+    return LayoutBuilder(
+      builder: (context, constraints) => Container(
+        height: 173.0,
+        width: constraints.maxWidth,
+        decoration: const BoxDecoration(
+          color: Color(0xff4B4B4B),
+          borderRadius: BorderRadius.all(
+            Radius.circular(8.0),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Text(
+                'Total gasto',
+                style: TextStyle(
+                  color: Color(0xffFFFFFF),
+                  fontFamily: 'Roboto',
+                  fontWeight: FontWeight.w400,
+                  fontStyle: FontStyle.normal,
+                  fontSize: 14.0,
+                ),
+              ),
+              SizedBox(height: 3),
+              Text(
+                '\$9900.97',
+                style: TextStyle(
+                  color: Color(0xffFFFFFF),
+                  fontFamily: 'Roboto',
+                  fontWeight: FontWeight.w400,
+                  fontStyle: FontStyle.normal,
+                  fontSize: 28.0,
+                ),
+              ),
+              SizedBox(height: 8),
+              Text(
+                'Esse mês você gastou \$1500.00 com jogos. Tente abaixar esse custo!',
+                style: TextStyle(
+                  color: Color(0xffFFFFFF),
+                  fontFamily: 'Roboto',
+                  fontWeight: FontWeight.w400,
+                  fontStyle: FontStyle.normal,
+                  fontSize: 15.0,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 14.0, horizontal: 8.0),
+                child: Text(
+                  'Diga-me como',
+                  style: TextStyle(
+                    color: Color(0xffB20CBB),                    
+                    fontFamily: 'Roboto',
+                    fontWeight: FontWeight.w400,
+                    fontStyle: FontStyle.normal,
+                    fontSize: 14.0,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
